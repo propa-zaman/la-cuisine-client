406b969a23779751914fb813273a54b05f6503ce
Afra <propazaman12@gmail.com>
2023-05-04 11:59:03 +0600

    some components and layout created

---
+++ src/Routes/Route.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import Main from '../Layout/Main';
+import Login from '../Components/Login';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+        ]
+    }
+])
+
+export default router;
