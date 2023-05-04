71dd31890907cecc4b87ce3d134914176fb778c3
Afra <propazaman12@gmail.com>
2023-05-04 12:11:21 +0600

    firebase added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCJBRc8G9ht8DpZ_P8kUJrnDMy1g651Pjw",
+  authDomain: "la-cuisine-537fb.firebaseapp.com",
+  projectId: "la-cuisine-537fb",
+  storageBucket: "la-cuisine-537fb.appspot.com",
+  messagingSenderId: "981003943746",
+  appId: "1:981003943746:web:298c6a43295405d13212db"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+export default app;
